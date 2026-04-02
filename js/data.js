bfa39dfca31c54ec7d249583baccbb0eefdfab05
jerry85de <jeremyeigner@googlemail.com>
2026-04-02 21:29:06 +0200

    Add functions to retrieve techniques and categories

--- js/data.js
+++ js/data.js
@@ -11,4 +11,18 @@ export function getBelts() {
 
 export function getBeltById(id) {
   return DATA.belts.find(b => b.id === id);
+
+export function getTechniquesByBelt(beltId) {
+  return DATA.techniques.filter(t =>
+    t.beltRefs.some(ref => ref.beltId === beltId)
+  );
+}
+
+export function getTechniqueById(id) {
+  return DATA.techniques.find(t => t.id === id);
+}
+
+export function getCategories() {
+  return DATA.categories;
+}
 }
