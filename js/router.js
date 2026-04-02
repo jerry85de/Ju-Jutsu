833b0f55d9ee4a85653ea4f49b07734fa4db4133
jerry85de <jeremyeigner@googlemail.com>
2026-04-03 00:12:27 +0200

    Delete js directory

--- js/router.js
+++
@@ -1,6 +0,0 @@
-export function getRoute(){
- const hash=window.location.hash;
- if(!hash)return{page:"home"};
- const parts=hash.replace("#/","").split("/");
- return{page:parts[0],id:parts[1]};
-}
